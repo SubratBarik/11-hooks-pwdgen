6522b7c1e127787dc1f312232bc2d44357d72ade
SubratBarik <subrat@blockstack.tech>
2025-08-04 20:10:10 +0530

    Hooks using generate PWD app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './assets/common.scss';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="main">
+          <h1>Password Generator using useEffect, useRef and useCallback Hooks</h1>
+          <h3>Hello World</h3>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
